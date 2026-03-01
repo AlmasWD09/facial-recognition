ed9d3e4a0135413e354ff8988346bbc38fd8b387
almas hossain <almashossain7384@gmail.com>
2026-03-01 09:00:01 +0600

    front end done

--- app/(authentication)/(webAuth)/auth/page.tsx
+++ app/(authentication)/(webAuth)/auth/page.tsx
@@ -1,9 +1,6 @@
 import LoginSuspense from '@/components/view/login'
 
-export const metadata = {
-    title: "Facial Recognition | Login",
-    description: "Log in to access services by Facial Recognition.",
-};
+
 
 
 const LoginPage = () => {
